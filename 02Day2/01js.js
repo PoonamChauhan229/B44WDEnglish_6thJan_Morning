480af253b4f6fe577453c7b407f9d0324e1ae9ad
Poonam <poonam.chauhan229@gmail.com>
2023-01-24 13:58:20 +0530

    day2

---
+++ 02Day2/01js.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+console.log("Welcome")
+
+// after 3 sec
+setTimeout(()=>{
+    console.log(113) 
+},3000)
+// console.log(113)
+
+console.log(1)
+console.log("1Hello1")
+console.log("1Welcome2")
